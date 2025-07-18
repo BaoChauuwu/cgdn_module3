8ccb529cbff9660d8e9e5c0b523bbacc7c02d6f3
BaoChauuwu <baochaudeptrai300@gmail.com>
2025-07-18 18:41:49 +0700

    update

--- bai3.sql
+++ bai3.sql
@@ -29,3 +29,63 @@ CREATE TABLE OrderDetail (
     FOREIGN KEY (oID) REFERENCES `Order`(oID),
     FOREIGN KEY (pID) REFERENCES Product(pID)
 );
+
+-- Insert Customer
+INSERT INTO Customer (cID, cName, cAge) VALUES
+(1, 'Minh Quan', 10),
+(2, 'Ngoc Oanh', 20),
+(3, 'Hong Ha', 50);
+
+-- Insert Product
+INSERT INTO Product (pID, pName, pPrice) VALUES
+(1, 'May Giat', 3),
+(2, 'Tu Lanh', 5),
+(3, 'Dieu Hoa', 7),
+(4, 'Quat', 1),
+(5, 'Bep Dien', 2);
+
+-- Insert Order
+INSERT INTO `Order` (oID, cID, oDate, oTotalPrice) VALUES
+(1, 1, '2006-03-21', NULL),
+(2, 2, '2006-03-23', NULL),
+(3, 1, '2006-03-16', NULL);
+
+-- Insert OrderDetail
+INSERT INTO OrderDetail (oID, pID, odQTY) VALUES
+(1, 1, 3),
+(1, 3, 7),
+(1, 4, 2),
+(2, 1, 1),
+(2, 5, 8),
+(3, 3, 3);
+
+SELECT o.oID, o.oDate, o.oTotalPrice 
+FROM `Order` AS o;
+
+
+SELECT 
+    c.cID,
+    c.cName,
+    p.pName,
+    od.odQTY
+FROM Customer AS c
+JOIN `Order` AS o ON c.cID = o.cID
+JOIN OrderDetail AS od ON o.oID = od.oID
+JOIN Product AS p ON od.pID = p.pID;
+
+
+
+SELECT c.cName 
+FROM Customer AS c
+LEFT JOIN `Order` AS o ON c.cID = o.cID
+WHERE o.oID is NULL;
+
+
+SELECT o.oID, o.oDate, SUM(od.odQTY * p.pPrice) AS total_price
+FROM `Order` AS o
+JOIN OrderDetail AS od ON o.oID = od.oID
+JOIN Product AS p ON od.pID = p.pID
+GROUP BY o.oID, o.oDate;
+
+
+
